4950df444e2e8098d57368c39529cb166f60658e
Electromaster232 <gilbert7573@gmail.com>
2019-12-11 15:31:00 -0500

    Initial Commit

---
+++ SoftwareDevelopment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftwareDevelopment
+//
+//  Created by Adam Gilbert (student HH) on 12/11/19.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
